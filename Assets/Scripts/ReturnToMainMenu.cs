89a13d333a52f4d421d35065530281b5d9f3709d
Seb Vowles <sebastian.vowles@Swansea.ac.uk>
2023-02-13 15:56:59 +0000

    fixed time setting bug

--- Assets/Scripts/ReturnToMainMenu.cs
+++ Assets/Scripts/ReturnToMainMenu.cs
@@ -9,6 +9,8 @@ public class ReturnToMainMenu : MonoBehaviour
 
     public OVRScreenFade ovrScreenFade;
 
+    //private GameObject gameObjectToDestroy;
+
     private void Awake()
     {
         ovrScreenFade = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<OVRScreenFade>();
@@ -23,15 +25,18 @@ public class ReturnToMainMenu : MonoBehaviour
     {
         ovrScreenFade.FadeOut();
         yield return new WaitForSeconds(2f);
+        GameObject gameObjectToDestroy = GameObject.FindGameObjectWithTag("SceneAndScoreManager");
+        Destroy(gameObjectToDestroy);
         if (demoMode)
         {
+
             SceneManager.LoadScene("Urdd_LanguageSelect");
 
         }
         else
-        {
-            SceneManager.LoadScene("SportScienceMainMenu_EnglishVersion");
+        {     
 
+            SceneManager.LoadScene("SportScienceMainMenu_EnglishVersion");
         }
     }
 }
